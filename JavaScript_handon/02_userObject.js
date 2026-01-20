789dd28c31fdb834cca2f61cd30a93ea142e6f2d
deepak-tigt <dkdodiya@trueigtech.com>
2026-01-20 11:45:24 +0530

    user object is created and manupulated

---
+++ JavaScript_handon/02_userObject.js
@@ -0,0 +1,11 @@
+// Hands-on: Create an object to store user details and manipulate it
+
+const userObject = {
+    username : "deepak",
+    age : "21",
+}
+
+console.log(userObject)
+userObject.username = "ajay"
+
+console.log(userObject)
